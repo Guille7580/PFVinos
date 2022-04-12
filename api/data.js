3985383d68218c14d2cdf1ee1b93b82f8a45a160
andrea198824 <andreguti88@gmail.com>
2022-04-11 19:08:15 -0500

    data

---
+++ api/data.js
@@ -0,0 +1,303 @@
+
+module.exports = [
+    {
+       "id": 1,
+       "title": "Zuccardi Finca Piedra Infinita Supercal Malbec",
+       "price": 400.90,
+       "description": "Este maravilloso vino está catalogado como “vino de suelo” por sus características especiales con manifestaciones de tiza, piedras húmedas y cal, que aportan el complemento perfecto al sabor de las uvas y aroma algo sobrios y moderados.",
+       "category": "Blanco",
+       "image": "https://s03.s3c.es/imag/_v0/770x420/9/f/6/mejor-vino.jpg",
+       "age": 2015,
+       "cepa":"Malbec",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 3.9,
+          "count": 120
+       }
+    },
+    {
+       "id": 2,
+       "title": "Alpha Estate Hedgehog Vineyard",
+       "price": 350.90,
+       "description": "Este es un rosado elegante y equilibrado, con aromas tranquilos de fresas y flores. Estas notas emiten un ambiente provenzal, pero este vino es decididamente griego.",
+       "category": "Rosado",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-50.jpg",
+       "age": 2009,
+       "cepa":"Malbec",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 3.7,
+          "count": 110
+       }
+    },
+    {
+       "id": 3,
+       "title": "Wenzlau Vineyard",
+       "price": 290.90,
+       "description": "Este maravilloso vino está catalogado como “vino de suelo” por sus características especiales con manifestaciones de tiza, piedras húmedas y cal, que aportan el complemento perfecto al sabor de las uvas y aroma algo sobrios y moderados.",
+       "category": "Tinto",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-49.jpg",
+       "age": 2012,
+       "cepa":"Cabernet",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 6.9,
+          "count": 125
+       }
+    },
+    {
+       "id": 4,
+       "title": "Mas De Daumas Gassac ‘Frizant’",
+       "price": 210.90,
+       "description": "Con un olor a rosas y fresas, este burbujeante francés tiene sabores distintivos de yogur cremoso.",
+       "category": "Rosado",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-48.jpg",
+       "age": 2019,
+       "cepa":"Sauvignon",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 3.9,
+          "count": 184
+       }
+    },
+    {
+       "id": 5,
+       "title": "Albert Bichot ‘Secret De Famille’ Bourgogne ",
+       "price": 430.90,
+       "description": "Un Chardonnay que es tan fresco y refrescante como profundo y delicioso, este vino no muestra signos de roble.",
+       "category": "Blanco",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-47.jpg",
+       "age": 2017,
+       "cepa":"Merlot",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 2.9,
+          "count": 104
+       }
+    },
+    {
+       "id": 6,
+       "title": "Enturini Baldini ‘Montelocco’ Lambrusco",
+       "price": 650.90,
+       "description": "Lambrusco es a menudo sacarina, pero cuando tiene un hilo prominente de sequedad para cortar esa dulzura, ha encontrado un vino adecuado en este estilo.",
+       "category": "Tinto",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-46.jpg",
+       "age": 2016,
+       "cepa":"Malbec",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 4.9,
+          "count": 120
+       }
+    },
+    {
+       "id": 7,
+       "title": "Kitá Spe’y Camp 4 Vineyard",
+       "price": 430.90,
+       "description": "De Tara Gomez, la enóloga del año de VinePair, llega este delicioso tinto de California.",
+       "category": "Tinto",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-45.jpg",
+       "age": 2016,
+       "cepa":"Sauvignon",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 3.9,
+          "count": 114
+       }
+    },
+    {
+       "id": 8,
+       "title": "Vivanterre ‘Msm’ ",
+       "price": 100.90,
+       "description": "En nariz, este funky vino francés huele a cuajada de limón salpicada de ralladura de limón.",
+       "category": "Blanco",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-44.jpg",
+       "age": 2020,
+       "cepa":"Merlot",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 5.9,
+          "count": 124
+       }
+    },
+    {
+       "id": 9,
+       "title": "Chapel Down Classic Brut Nv",
+       "price": 360.90,
+       "description": "Este vino espumoso inglés huele a manzanas verdes crujientes y afrutadas, con notas de hinojo dulce machacado al final.",
+       "category": "Tinto",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-43.jpg",
+       "age": 2005,
+       "cepa":"Malbec",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 2.9,
+          "count": 134
+       }
+    },
+    {
+       "id": 10,
+       "title": "David Hill Estate ‘First Pick’ Riesling",
+       "price": 450.90,
+       "description": "Desde el valle de Willamette en Oregón llega este conmovedor vino blanco. Tiene una agradable profundidad de fruta, la acidez es baja y huele a almendras, miel y peras.",
+       "category": "Blanco",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-42.jpg",
+       "age": 2019,
+       "cepa":"Merlot",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 6.9,
+          "count": 114
+       }
+    },
+    {
+       "id": 11,
+       "title": "My Favorite Neighbor ",
+       "price": 380.90,
+       "description": "Con aromas a fresas trituradas y demasiado maduras, este vino espumoso es muy refrescante y al mismo tiempo embriagador y profundo.",
+       "category": "Tinto",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-41.jpg",
+       "age": 2019,
+       "cepa":"Cabernet",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 6.9,
+          "count": 120
+       }
+    },
+    {
+       "id": 12,
+       "title": "Giant Steps Winery",
+       "price": 190.90,
+       "description": "Agradable, rico y aromático en nariz, este Chardonnay australiano huele ligeramente a peras caramelizadas y mantequilla fresca.",
+       "category": "Blanco",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-40.jpg",
+       "age": 2020,
+       "cepa":"Chardonnay",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 5.9,
+          "count": 124
+       }
+    },
+    {
+       "id": 13,
+       "title": "Scribe Pinot Noir",
+       "price": 230.90,
+       "description": "Untuoso y afrutado a medida que se abre, este vino está lleno de aromas profundos y maduros de fresas brillantes, cerezas y arándanos.",
+       "category": "Tinto",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-39.jpg",
+       "age": 2018,
+       "cepa":"Malbec",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 5.9,
+          "count": 124
+       }
+    },
+    {
+       "id": 14,
+       "title": "Locelee",
+       "price": 530.90,
+       "description": "Brillante y rico, este vino se abre con un toque de cereza, canela terrosa, un toque de frambuesa y una pizca de pimienta.",
+       "category": "Tinto",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-38.jpg",
+       "age": 2019,
+       "cepa":"Pinot",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 5.9,
+          "count": 124
+       }
+    },
+    {
+       "id": 15,
+       "title": "Vox Vineti",
+       "price": 170.90,
+       "description": "Junto con aromas de regaliz y un toque de sandías y cerezas, este vino huele como el suelo de un bosque terroso.",
+       "category": "Tinto",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-37.jpg",
+       "age": 2018,
+       "cepa":"Malbec",
+       "bodega": "Nebbiolo",
+       "rating": {
+          "rate": 5.9,
+          "count": 124
+       }
+    },
+    {
+       "id": 16,
+       "title": "Pierre Sparr",
+       "price": 120.90,
+       "description": "Con aromas a fresas trituradas y demasiado maduras, este vino espumoso es muy refrescante y al mismo tiempo embriagador y profundo.",
+       "category": "Rosado",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-36.jpg",
+       "age": 2015,
+       "cepa":"Crémant d'Alsace",
+       "bodega": "Nv",
+       "rating": {
+          "rate": 9.2,
+          "count": 124
+       }
+    },
+    {
+       "id": 17,
+       "title": "Gundlach-Bundschu",
+       "price": 310.90,
+       "description": "Para un Chardonnay estilo Cali de la vieja escuela, este vino tiene un excelente equilibrio. Huele a peras, un tufillo a mantequilla y algo de roca mojada.",
+       "category": "Blanco",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-35.jpg",
+       "age": 2019,
+       "cepa":"Chardonnay",
+       "bodega": "Estate Vineyard",
+       "rating": {
+          "rate": 7.9,
+          "count": 154
+       }
+    },
+    {
+       "id": 18,
+       "title": "Cantine",
+       "price": 470.80,
+       "description": "Fruta y tierra en el paladar, con una acidez brillante que anuncia el olor a ciruelas y cerezas oscuras, este vino es un ejemplo superior del estilo.",
+       "category": "Tinto",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-34.jpg",
+       "age": 2019,
+       "cepa":"Sauvignon",
+       "bodega": "Agriolas Costera",
+       "rating": {
+          "rate": 8.9,
+          "count": 84
+       }
+    },
+    {
+       "id": 19,
+       "title": "Long Meadow",
+       "price": 260.90,
+       "description": "Este vino huele a peras crujientes recién cortadas y pétalos de flores blancas. En boca tiene un gran equilibrio, con una acidez natural activa y sinuosa en todo momento.",
+       "category": "Blanco",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-33.jpg",
+       "age": 2018,
+       "cepa":"Pinot",
+       "bodega": "Zuccardi",
+       "rating": {
+          "rate": 7.9,
+          "count": 184
+       }
+    },
+    {
+       "id": 20,
+       "title": "C.L. Butaud Desert Willow",
+       "price": 780.90,
+       "description": "Agradable, maduro y sabroso, este Texas Mourvedre huele a guindas con una pizca de canela.",
+       "category": "Tinto",
+       "image": "https://vinepair.com/wp-content/uploads/2021/11/50-best-wines-2021_internal_wine-32.jpg",
+       "age": 2019,
+       "cepa":"Malbec",
+       "bodega": "Vineyard",
+       "rating": {
+          "rate": 6.2,
+          "count": 132
+       }
+    },
+ ]
