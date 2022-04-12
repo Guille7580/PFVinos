7357bea12773ab91dccf4bbb7fc1461ba3b70089
jennyxlombardo <78739007+jennyxlombardo@users.noreply.github.com>
2022-04-12 17:44:51 -0300

    Div

--- client/src/Pages/Home/home.jsx
+++ client/src/Pages/Home/home.jsx
@@ -29,6 +29,9 @@ function Home () {
         >
           Bienvenidas a Las Moritas
         </AnimatedText>
+        <div className='containerBody'>
+          
+        </div>
       <Footer />
     </>
   )
