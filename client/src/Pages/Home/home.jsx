3788d8952f7fffd4a11401dda9369ae33419dc14
jennyxlombardo <78739007+jennyxlombardo@users.noreply.github.com>
2022-04-12 16:58:24 -0300

    Home/NavBar/ShoppingButton/Footer

--- client/src/Pages/Home/home.jsx
+++ client/src/Pages/Home/home.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
-// import Footer from '../../components/Footer/footer'
-// import NavBar from '../../components/NavBar/navBar'
+import Footer from '../../components/Footer/footer'
+import NavBar from '../../components/navBar/navBar'
+import './home.css'
+import image from './PurpleGrapesSmall.jpg'
+import AnimatedText from 'react-animated-text-content'
 
 function Home () {
   return (
     <>
-      {/* <NavBar />
-      <Footer /> */}
+      <NavBar />
+      <img className='imageHome' src={image} alt="" />
+      <AnimatedText
+          type='words' // animate words or chars
+          animation={{
+            x: '200px',
+            y: '-20px',
+            scale: 1.1,
+            ease: 'ease-in-out'
+          }}
+          animationType='float'
+          interval={0.06}
+          duration={2.5}
+          tag='p'
+          className='animatedParagraph'
+          includeWhiteSpaces
+          threshold={0.1}
+          rootMargin='20%'
+        >
+          Bienvenidas a Las Moritas
+        </AnimatedText>
+      <Footer />
     </>
   )
 }
