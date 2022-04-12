b5a9ff666340cd4e85cafbdd7823b5f044e447cd
Javier Irigoyen Terre <javier_irigoyen96@hotmail.com>
2022-04-12 17:31:04 -0300

    cards y detail

---
+++ client/src/Detail.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import {Link} from "react-router-dom"
+import {useDispatch, useSelector} from "react-redux"
+import { useEffect } from "react"
+
+export default function Detail() {
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
