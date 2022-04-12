433af07cb792f083c249acfae21dcaf233e9c67e
Luciana Zanetti <luciana.zeta@gmail.com>
2022-04-12 12:06:46 -0300

    features/GetProductos
    Modelo Producto/ Categoria
    Ruta GET de productos

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,25 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const { Sequelize } = require('sequelize')
+const { Category, Product } = require('../db')
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+
+
+//------------------- product -----------------------------------------
+var product = require('./product')
+
+router.get('/products', product.getAllProducts)
+
+
+
+
+
 
 
 module.exports = router;
