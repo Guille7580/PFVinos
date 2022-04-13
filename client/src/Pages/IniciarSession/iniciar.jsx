e64235fed4f2e083f9a5b636158bdf30ac074847
jennyxlombardo <78739007+jennyxlombardo@users.noreply.github.com>
2022-04-13 20:01:58 -0300

    Arreglo

--- client/src/Pages/IniciarSession/iniciar.jsx
+++ client/src/Pages/IniciarSession/iniciar.jsx
@@ -6,6 +6,7 @@ import Footer from '../../components/Footer/footer'
 export default function IniciarSession () {
   return (
       <div>
+          {/* /*Hola */}
       <NavBar />
     <div className='iniciar'>
       <h1>Iniciar Session</h1>
