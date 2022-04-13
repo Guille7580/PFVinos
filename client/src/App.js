d8d0b1d829e29b8b8d36eee7fbfb11e089436020
jennyxlombardo <78739007+jennyxlombardo@users.noreply.github.com>
2022-04-13 08:17:33 -0300

    cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './Pages/Home/home.jsx'
+import Cart from './Pages/Checkout/Cart'
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/carrito' element={<Cart />} />
         </Routes>
       </BrowserRouter>
     </div>
