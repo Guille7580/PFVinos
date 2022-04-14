49fabd5ad9d3e14f9fb8b99d0be82d1b8608a368
Javier Irigoyen Terre <javier_irigoyen96@hotmail.com>
2022-04-14 15:51:04 -0300

    componente detail

--- client/src/components/Detail/detail.jsx
+++ client/src/components/Detail/detail.jsx
@@ -1,36 +1,46 @@
 import React from "react";
-import {  useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getDetail } from "../../actions/productos";
-import "./detail.css"
+import "./detail.css";
 
 export default function Detail() {
-     const dispatch = useDispatch()
-    const {id} = useParams()
+  const dispatch = useDispatch();
+  const { id } = useParams();
 
-    useEffect(() => {
-        dispatch(getDetail(id));
-      }, [dispatch]);  
+  useEffect(() => {
+    dispatch(getDetail(id));
+  }, [dispatch]);
 
-    const myProducts= useSelector((state) => state.productosReducer.detalles);
-      console.log(myProducts)
-return (    
-           
-        <div className="hola">
-    
-           {Object.keys(myProducts).length > 0 ? (
-              <div>
-                  <h1>{myProducts.title}</h1>
-                  <img src={myProducts.image} alt="img not found" />
-                  <h3>{myProducts.descriptions}</h3>
-                  <h5>{myProducts.price}</h5>
-            </div>
-         
-          ) : console.log("error")
-          
-          }   
+  const myProducts = useSelector((state) => state.productosReducer.detalles);
+  console.log(myProducts);
+  return (
+    <div className="hola">
+      {Object.keys(myProducts).length > 0 ? (
+        <div>
+          <h1>{myProducts.title}</h1>
+          <img src={myProducts.image} alt="img not found" />
+          <h3>{myProducts.descriptions}</h3>
+          <h3>{myProducts.bodega}</h3>
+          <h3>{myProducts.cepa}</h3>
+          <h3>{myProducts.age}</h3>
+          <h3>{myProducts.stock}</h3>
+          <h3>{myProducts.rate}</h3>
+          <h3>{myProducts.count}</h3>
+          <h5>{myProducts.price}</h5>
         </div>
-          
-    )
-        }
+      ) : (
+        console.log("error")
+      )}
+      <Link to="/">
+        <button>Keep Buying</button>
+      </Link>
+
+      <Link to="/carrito">
+        <button>Add To Cart</button>
+      </Link>
+    </div>
+  );
+}
