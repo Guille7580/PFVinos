23c6005512450edb72cae51b6575c86f16109fff
Guille7580 <guilleappen@hotmail.com>
2022-04-15 17:29:40 -0300

    control email , tel y alerta en falla de registro mas action y , archivo reduser

--- client/src/Pages/Register/register.jsx
+++ client/src/Pages/Register/register.jsx
@@ -3,10 +3,10 @@ import './register.css'
 import { useDispatch } from 'react-redux'
 import NavBar from '../../components/navBar/navBar'
 import { useNavigate } from 'react-router-dom'
-//import Swal from 'sweetalert2'
-import { validateEmail, validateTlf } from '../Helpers/validateForm'
+import Swal from 'sweetalert2'
 // import { auth, provider } from '../Helpers/validateAuth'
 import { postUser } from '../../actions/user'
+import {validateForm} from './control registro/validate'
 
 
 // const initialForm = {
@@ -21,56 +21,6 @@ import { postUser } from '../../actions/user'
 //   telefono: ''
 // }
 
-const validateForm = function (input) {
-  const errors = {}
-  if (!input.nombre.trim()) {
-    errors.nombre = 'Campo requirido'
-  } else if (input.nombre.length < 4) {
-    errors.nombre = 'Minimo 4 caracters'
-  } else if (input.nombre > 25) {
-    errors.nombre = 'Maximo 25 caracters'
-  }
-
-  if (!input.usuario.trim()) {
-    errors.usuario = 'Campo requirido'
-  } else if (input.usuario.length < 5) {
-    errors.usuario = 'Minimo 5 caracters'
-  } else if (input.usuario.length > 25) {
-    errors.usuario = 'Maximo 25 caracters'
-  }
-  if (!input.contrasena.trim()) {
-    errors.contrasena = 'Campo requerido'
-  } else if (input.contrasena.length < 10) {
-    errors.contrasena = 'Mínimo 10 caracteres'
-  }
-  if (!input.email.trim()) {
-    errors.email = 'Campo requerido'
-  } else if (!validateEmail(input.email)) {
-    errors.email = 'Escriba un email válido'
-  }
-  if (!input.pais.trim()) {
-    errors.pais = 'Campo requerido'
-  }
-  if (!input.provincia.trim()) {
-    errors.provincia = 'Campo requerido'
-  }
-  if (!input.direccion.trim()) {
-    errors.direccion = 'Campo requerido'
-  } else if (input.direccion.length < 5) {
-    errors.direccion = 'Mínimo 5 caracteres'
-  } else if (input.direccion.length > 40) {
-    errors.direccion = 'Máximo 40 caracteres'
-  }
-  if (!input.telefono.trim()) {
-    errors.telefono = 'Campo requerido'
-  } else if (!validateTlf(input.telefono)) {
-    errors.telefono = 'Escriba un número de telefono válido'
-  }
-  if (input.confirm_contrasena !== input.contrasena) {
-    errors.confirm_contrasena = 'Las contraseñas no coinciden'
-  }
-  return errors
-}
 
 export default function Register ({ edit = false }) {
   const dispatch = useDispatch()
@@ -141,7 +91,12 @@ export default function Register ({ edit = false }) {
       })
       navigate('/')
     } else {
-      alert('Completar el Formulario')
+      Swal.fire({
+        text: `Completar el Formulario`,
+        icon: "error",
+        confirmButtonText: "Ok",
+      });
+      // alert('Completar el Formulario')
     }
   }
 
