f2840f771980f01c613e50cfc4f59aa9a8bcb41b
Jennifer Lombardo <jenniferxlombardo@gmail.com>
2022-04-21 14:31:32 -0300

    Revert "ArreglosCarrito/Items"

--- client/src/Pages/Checkout/Cart.jsx
+++ client/src/Pages/Checkout/Cart.jsx
@@ -2,27 +2,25 @@ import React, { useEffect } from 'react'
 //import { useSelector, useDispatch } from 'react-redux'
 import './Cart.css'
 import NavBar from '../../components/navBar/navBar'
-import { Link } from 'react-router-dom'
+//import { useNavigate } from 'react-router-dom'
 //import { Loader } from '../../components/Loader/Loader'
 //import { updateCart, getCartDb, deleteAllCart, deleteAllCartDB } from '../../actions/carrito'
-import Swal from 'sweetalert2'
-import CartItems from './CartItems/CartItems'
+import Swal from "sweetalert2"
+import CartItems from "./CartItems"
 
-export function calculateTotal (items) {
+export function calculateTotal(items) {
   return items
-    ?.reduce((acc, item) => acc + item.amount * item.price, 0)
-    .toFixed(2)
+    ?.reduce(
+      (acc, item) =>
+        acc + item.amount * item.price,
+      0
+    )
+    .toFixed(2);
 }
 
-export default function Cart ({
-  handleAddToCart,
-  cartItems,
-  setCartItems,
-  handleRemoveFromCart,
-  handleDeleteFromCart,
-  getTotalItems
-}) {
-  /* const navigate = useNavigate();
+
+export default function Cart ({handleAddToCart,  cartItems, setCartItems, handleRemoveFromCart, handleDeleteFromCart, getTotalItems}) {
+ /* const navigate = useNavigate();
   const dispatch = useDispatch();
 
  let items = useSelector((state) => {
@@ -43,34 +41,35 @@ items = items?.filter((e) => e);
  const user = useSelector((state) => state.loginReducer.userDetail)
  const cartDB = useSelector((state) => state.productsReducer.carts) */
   return (
+
     <div>
       <div div className='cartContainer'>
         <h1>Shopping Cart</h1>
+        <div className='totalContainer'>
+          <h2>Total: &nbsp; </h2>
+          <h3>${calculateTotal(cartItems)}</h3>
+        </div>
         <div className='cartButtons'>
-          <div className='cartButton'></div>
+          <div className='cartButton'>
+          
+          </div>
         </div>
         <div>
-          <ul>
-            {cartItems?.length === 0 ? <p>No hay items en el carrito</p> : null}
-            {cartItems?.map(product => (
-              <CartItems
-                key={product.id}
-                product={product}
-                handleAddToCart={handleAddToCart}
-                handleRemoveFromCart={handleRemoveFromCart}
-                handleDeleteFromCart={handleDeleteFromCart}
-              />
-            ))}
-          </ul>
-          <div className='totalContainer'>
-            <Link to='/'>
-              <button className='btnBottom'>Seguir Comprando</button>
-            </Link>
-            <h2>Total: &nbsp; ${calculateTotal(cartItems)} </h2>
-            <Link to="#">
-            <button className='btnBottom'>Pagar</button>
-            </Link>
-          </div>
+        <ul>
+                          {cartItems?.length === 0 ? (
+                            <p>No hay items en el carrito</p>
+                          ) : null}
+                          {cartItems?.map((product) => (
+                            <CartItems
+                              key={product.id}
+                              product={product}
+                              handleAddToCart={handleAddToCart}
+                              handleRemoveFromCart={handleRemoveFromCart}
+                              handleDeleteFromCart={handleDeleteFromCart}
+                              
+                            />
+                          ))}
+                        </ul>
         </div>
       </div>
     </div>
