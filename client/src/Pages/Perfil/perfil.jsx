f09885051bb9352bac583aacca6d780692f39ebb
jennyxlombardo <78739007+jennyxlombardo@users.noreply.github.com>
2022-04-22 07:40:31 -0300

    perfil

---
+++ client/src/Pages/Perfil/perfil.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './perfil.css'
+
+
+export default function Perfil() {
+
+    return (
+        <div className="perfilContainer">
+      
+            Hola, 
+        </div>
+    )
+}
