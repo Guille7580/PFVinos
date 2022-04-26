8ef54f6dcfd88d909a66a6d802065866fcb51a03
Andrea Gutierrez <andreguti88@gmail.com>
2022-04-26 07:16:54 -0500

    Revert "Merge pull request #77 from Guille7580/fixes"
    This reverts commit d94641f50a24831d6fa47a8195ffa544d79f289e, reversing
    changes made to 90848a30a0dc8dbd5a77b0a4cf9321a74445a9ce.

--- client/src/Pages/Checkout/CartItems/CartItems.jsx
+++ client/src/Pages/Checkout/CartItems/CartItems.jsx
@@ -20,20 +20,20 @@ const CartItems = ({
         <h4>Cantidad: {product.amount}</h4>
       </div>
       <div className='btnMasMenos'>
-        <button
-          className='btnItems1'
-          onClick={() => handleRemoveFromCart(product.id)}
-        >
-          -
+        <button className='btnItems' onClick={() => handleAddToCart(product)}>
+          +
         </button>
         <button
-          className='btnItems2'
+          className='btnItems'
           onClick={() => handleDeleteFromCart(product.id)}
         >
           Borrar
         </button>
-        <button className='btnItems3' onClick={() => handleAddToCart(product)}>
-          +
+        <button
+          className='btnItems'
+          onClick={() => handleRemoveFromCart(product.id)}
+        >
+          -
         </button>
       </div>
       <div className='subtotalItem'>
