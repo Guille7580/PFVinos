4b04734504b04290b83d053e5c6be1966b65bdc7
Guille7580 <guilleappen@hotmail.com>
2022-05-02 20:51:32 -0300

    reset user

--- api/src/controllers/resetPassword.js
+++ api/src/controllers/resetPassword.js
@@ -1,38 +1,42 @@
 const { User } = require("../db");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs"); //encriptar contraseña
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { JWT_SECRET } = process.env
 
-let regExPassword =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@!%*?&#])[A-Za-z\d@$!%*?&#]{8,16}$/;
 
 const ResetPassword = async (req, res, next) => {
-  if (!regExPassword.test(req.body.contrasena)) {
-    res.status(400).json({
-      message:
-        "La contraseña deberia contener al menos: 8 caracteres, 1 numero, 1  minuscula, 1 mayuscula y 1 caracter especial",
-    });
-    return;
+  const {id , contrasena, email } = req.body.userForm
+  console.log(email)
+
+  if (!email) {
+    return next({ status: 400, message: "Email is required" })
   }
 
+  
+  let temppasAct = await bcrypt.hash(contrasena , 10);
+
   try {
-    const contrasena = await bcrypt.hash(req.body.contrasena, 10);
-    const resetPassword = await User.update(
-      { contrasena },
-      {
-        where: {
-          id: req.params.id,
-        },
+    const user = await User.findOne({ where: { id } })
+    console.log(user.email)
+    
+    if (user) {
+      try {
+        await User.update({ contrasena: temppasAct }, { where: { email } })
+        
+            res.status(200).json("contraseña actualizada");
+      }catch (error) {console.log(error);
+        return next({ status: 500, message: "Intentelo más tarde" })
       }
-    );
-    res.status(201).json({
-      message: "contraseña cambiada con exito",
-      data: resetPassword,
-    });
+    }
+
+    res.json({
+      message: "Contraseña actualizada correctamente",
+    })
   } catch (error) {
-    res.status(500).json({
-      message: "este error",
-      error,
-    });
+    next(error)
   }
-};
+}
 
 module.exports = { ResetPassword };
