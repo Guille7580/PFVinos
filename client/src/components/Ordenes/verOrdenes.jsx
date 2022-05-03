095dac184ed7e778e2e6494a3e24163f7ad6f5f4
jennyxlombardo <78739007+jennyxlombardo@users.noreply.github.com>
2022-05-03 11:06:11 -0300

    ok

--- client/src/components/Ordenes/verOrdenes.jsx
+++ client/src/components/Ordenes/verOrdenes.jsx
@@ -6,21 +6,21 @@ import { getMaxListeners } from 'process'
 import pedidosReducer from '../../reducer/pedidos'
 
 export default function VerOrdenes () {
-  const [data, setData] = useState({})
-const [ state, dispatch ] = useReducer(pedidosReducer, userPedidos )
-  //const dispatch = useDispatch()
+//   const [data, setData] = useState({})
+// const [ state, dispatch ] = useReducer(pedidosReducer, userPedidos )
+//   //const dispatch = useDispatch()
 
-  useEffect(() => {
-    let email = 'luis@gmail.com' || "rodrigo.lopez@gmail.com"
-    dispatch(getPedidosByUser(email))
-  }, [dispatch])
-  const clientPedidos = useSelector(state => state.pedidosReducer.userPedidos)
-console.log(JSON.stringify(clientPedidos) + "LLEgaste")
-  useEffect(() => {
-    setData(clientPedidos)
-  }, [clientPedidos])
+//   useEffect(() => {
+//     let email = 'luis@gmail.com' || "rodrigo.lopez@gmail.com"
+//     dispatch(getPedidosByUser(email))
+//   }, [dispatch])
+//   const clientPedidos = useSelector(state => state.pedidosReducer.userPedidos)
+// console.log(JSON.stringify(clientPedidos) + "LLEgaste")
+//   useEffect(() => {
+//     setData(clientPedidos)
+//   }, [clientPedidos])
 
-   console.log(data + ' hhhhhhhhhhhhhhhhhh')
-  console.log(JSON.stringify(clientPedidos) + 'hollllissssssssssss')
-  return <div>{clientPedidos}</div>
+//    console.log(data + ' hhhhhhhhhhhhhhhhhh')
+//   console.log(JSON.stringify(clientPedidos) + 'hollllissssssssssss')
+//    return <div>{clientPedidos}</div>
 }
