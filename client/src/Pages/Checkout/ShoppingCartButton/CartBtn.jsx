d7b8c0f48aaffa402d1fcd4d9323b450182d84e0
Andrea Gutierrez <andreguti88@gmail.com>
2022-05-03 15:34:55 -0500

    Revert "Merge branch 'Dev' into feature/pago"
    This reverts commit 053eee4094453dcf3fb2249c57c265ba7ace3e9b, reversing
    changes made to 3156a348562d5eeb58a5875584235a2e0c3e56ae.

--- client/src/Pages/Checkout/ShoppingCartButton/CartBtn.jsx
+++ client/src/Pages/Checkout/ShoppingCartButton/CartBtn.jsx
@@ -8,17 +8,31 @@ import { BsCart3 } from "react-icons/bs";
 
 function CartBtn() {
   const [items, setItems] = useState([]);
-  const json = JSON.parse(localStorage.getItem("carrito"));
-  const [contador, setContador] = useState(0);
-
-  useEffect(() => {
+   useEffect(() => {
     setItems(JSON.parse(localStorage.getItem("carrito")));
-  }, [json]);
+  }, []);
+  const json= JSON.parse(localStorage.getItem("carrito")); 
+  const [contador, setContador] = useState(0)
+ /*  if(items && items.length > 0) {
+    console.log(items)
+  for (let i = 0; i < [...items].length; i++) {
+    setContador(contador + items[i].amount)
+
+  }
+  }  */
+  /* items.length > 0
+      ? items.reduce((a, b) => ({ amountTotal: a.amount + b.amount }))
+      : []; */
+  
 
   return (
     <div>
-      <BsCart3 />
-      {json?.length}
+    {/*   { items.length > 0
+      ? items.reduce((a, b) => ({ amountTotal: a.amount + b.amount })).amountTotal
+      : [] } */}
+      {/* <img src={cart} className='cartButton' alt='icon of a cart' /> */}
+      <BsCart3 /> 
+    {json?.length}
     </div>
   );
 }
