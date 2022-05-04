009adfdaae7de058de33eddef365b63068e4e127
jennyxlombardo <78739007+jennyxlombardo@users.noreply.github.com>
2022-05-04 20:16:26 -0300

    Update verOrdenes.jsx

--- client/src/components/Ordenes/verOrdenes.jsx
+++ client/src/components/Ordenes/verOrdenes.jsx
@@ -1,20 +1,53 @@
 import React, { useState } from 'react'
 import { getPedidosByUser } from '../../actions/pedidos'
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-export default function VerOrdenes(){
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(getPedidosByUser());
-    }, [dispatch]);
+export default function VerOrdenes () {
+  const dispatch = useDispatch()
+  const user = useSelector(state => state.loginReducer.userDetail)
+  const pedidos = useSelector(state => state.pedidosReducer.userPedidos)
 
-    const allOrders = useSelector(state => state.userPedidos)
-console.log(allOrders + "hollllissssssssssss")
-    return (
+  console.log(pedidos)
+
+  useEffect(() => {
+    dispatch(getPedidosByUser(user.email))
+  }, [user])
+
+  const pepe = pedidos.data
+    ?.map(el => el.products.map(ell => JSON.parse(ell)))
+    .flat()
+    .map(elll => elll.title)
+  console.log(pepe)
+  return (
+    <div>
+      {
         <div>
-
-
+          {pepe.map(el => (
+            <div>{el}</div>
+          ))}
         </div>
-    )
+      }
+    </div>
+  )
 }
+
+// import React, { useState } from 'react'
+// import { getPedidosByUser } from '../../actions/pedidos'
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+
+// export default function VerOrdenes(){
+//     const dispatch = useDispatch()
+//     useEffect(() => {
+//         dispatch(getPedidosByUser());
+//     }, [dispatch]);
+
+//     const allOrders = useSelector(state => state.userPedidos)
+// console.log(allOrders + "hollllissssssssssss")
+//     return (
+//         <div>
+
+//         </div>
+//     )
+// }
