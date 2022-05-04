c43ba10c5adf1b0fbebb1bc48d293b650730c319
Guille7580 <guilleappen@hotmail.com>
2022-05-03 22:25:14 -0300

    -

--- client/src/assets/URLS.js
+++ client/src/assets/URLS.js
@@ -1,6 +1,6 @@
 
-//export const BASEURL ="https://ecommerce-pg-henry.herokuapp.com";
-export const BASEURL ="http://localhost:3001";
+export const BASEURL ="https://deploy-app-vinos.herokuapp.com";
+// export const BASEURL ="http://localhost:3001";
 
 // export const LOGIN_URL = `${BASEURL}/user/login`;
 // export const REGISTER_URL = `${BASEURL}/user/register`;
